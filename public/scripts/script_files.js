896810c136e4aef4bb7ac2966ac7f8e635eee30a
joaopa24 <joaokn129@gmail.com>
2021-04-01 21:05:12 -0300

    haslimit

--- public/scripts/script_files.js
+++ public/scripts/script_files.js
@@ -4,5 +4,31 @@ const PhotosUpload = {
     uploadLimit:1,
     files:[],
 
+    HasLimit(event){
+        const { uploadLimit, input, preview } = PhotosUpload
+        const {files: fileList} = input
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie apenas ${uploadLimit} imagem!`)
+            event.preventDefault()
+            return true
+        }
+
+        const photosDiv
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == "photo") {
+                photosDiv.push(item)
+            }
+        })
+
+        const totalPhotos = fileList.length + photosDiv.length
+        if (totalPhotos > uploadLimit) {
+            alert(`Você atingiu o máximo de fotos`)
+            event.preventDefault()
+            return true
+        }
+
+        return false
+    },
     
 }
