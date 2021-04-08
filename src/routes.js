82bebff1ef391653014d169ebe4db6c7953ad530
joaopa24 <joaokn129@gmail.com>
2021-04-08 18:53:13 -0300

    multer array photos post

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('./app/middlewares/multer')
 const recipes = require('./app/controllers/recipes')
 const chef = require('./app/controllers/chefs')
 
@@ -14,9 +15,9 @@ routes.get("/admin/Chefs", chef.chefsAdmin)
 routes.get("/admin/Chefs/criar", chef.chefsCreate)
 routes.get("/admin/Chefs/:id", chef.chefAdmin)
 routes.get("/admin/Chefs/:id/edit", chef.chefAdmin_edit)
-routes.post("/admin/Chefs", chef.post)
-routes.delete("/admin/Chefs", chef.delete)
+routes.post("/admin/Chefs", multer.array("photos", 1),chef.post)
 routes.put("/admin/Chefs", chef.put)
+routes.delete("/admin/Chefs", chef.delete)
 
 
 routes.get("/admin/Receitas", recipes.index)
