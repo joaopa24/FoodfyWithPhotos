bfca63320d4532441a339175765899b54bf2af12
joaopa24 <joaokn129@gmail.com>
2021-04-09 16:15:13 -0300

    add photos in fields

--- src/routes.js
+++ src/routes.js
@@ -23,8 +23,8 @@ routes.get("/admin/Receitas", recipes.index)
 routes.get("/admin/Receitas/criar", recipes.create)
 routes.get("/admin/Receitas/:id", recipes.recipe_admin)
 routes.get("/admin/Receitas/:id/edit", recipes.recipe_admin_edit)
-routes.post("/admin/Receitas", multer.array("photos", 3),recipes.post)
-routes.put("/admin/Receitas", multer.array("photos", 3),recipes.put)
+routes.post("/admin/Receitas", multer.array("photos", 5),recipes.post)
+routes.put("/admin/Receitas", multer.array("photos", 5),recipes.put)
 routes.delete("/admin/Receitas", recipes.delete)
 
 module.exports = routes
