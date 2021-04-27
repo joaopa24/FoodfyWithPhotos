ddab1a7ebdd3690b1118bf7a7e340c9d0b000735
joaopa24 <joaokn129@gmail.com>
2021-04-27 16:26:43 -0300

    removed_files put

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -194,6 +194,16 @@ module.exports = {
                   await Promise.all(newFilesPromise)
               }
         }
+
+        if(req.body.removed_files){
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
         
         await Recipe.update(req.body)
 
